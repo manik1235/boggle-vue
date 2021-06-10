744caaa23a883ac61a7b732f471a6aec79133104
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2021-06-09 20:09:36 -0400

    Create an empty Boggle Board

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BoggleBoard></BoggleBoard>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoggleBoard from './components/BoggleBoard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BoggleBoard
   }
 }
 </script>
